5b9e59fda97c8f92f6a0226b4a652fd6896964a0
Olid <oliddev@olid.dev>
2026-04-03 17:18:15 +0700

    update mobile

--- header.php
+++ header.php
@@ -4,14 +4,14 @@
         <!-- Overlay transparan -->
         <div class="carousel-overlay"></div>
 
-        <div class="carousel-caption">
+        <div class="carousel-caption py-2">
             <div class="container">
                 <div class="row g-4 align-items-center">
                     <div class="col-lg-12 animated wow fadeInTop text-center" data-wow-delay="0.2s">
                         <div class="text-sm-center">
                             <h4 class="text-white text-uppercase fw-bold mb-4 wel"><?= lang('Welcome To') ?> Selaras Recruitment</h4>
                             <h1 class="display-1 text-white mb-4"><?= lang('Connecting Great People With Great Companies') ?></h1>
-                            <p class="mb-5 fs-5 we">
+                            <p class="mb-2 fs-5 we">
                                 <?= lang('We help organizations find the right talent and empower professionals to achieve their career goals. With a strategic and personalized approach, Selaras Recruitment connects great people with great companies for long-term success.') ?>
                             </p>
                             <div class="d-flex justify-content-center justify-content-md-center flex-shrink-0 mb-4">
